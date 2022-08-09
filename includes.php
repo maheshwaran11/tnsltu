f3d9bc5e93bcbbcdbc43d48ed149d2c8c8036a4e
maheshwaran11 <38691303+maheshwaran11@users.noreply.github.com>
2022-08-09 10:55:02 +0530

    Add files via upload

---
+++ includes.php
@@ -0,0 +1,11 @@
+<?php $BASE = 'http://localhost/trust/' ?>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-state=1.0"/>
+<title>TNSLTU</title>
+<link rel="stylesheet" href="<?php echo $BASE?>dist/css/bootstrap.css" />
+<script src="<?php echo $BASE?>dist/js/jquery-2.2.0.min.js" type="text/javascript"></script>
+<script src="<?php echo $BASE?>dist/js/script.js" type="text/javascript"></script>
+<!-- <script src="<?php echo $BASE ?>dist/js/jquery.dataTables.min.js" type="text/javascript"></script> -->
+<link href="<?php echo $BASE?>dist/css/icon.css" rel="stylesheet">
+<link id="pagestyle" href="<?php echo $BASE?>dist/css/material-dashboard.css?v=3.0.2" rel="stylesheet" />
+<link rel="stylesheet" href="<?php echo $BASE?>style.css" />
